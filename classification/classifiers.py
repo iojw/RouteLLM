fec8d71e42be11db9014313c9a5e7cbf29a0b1e1
Wei-Lin Chiang <weichiang@berkeley.edu>
2023-11-01 02:37:21 +0000

    add gpt

--- classification/classifiers.py
+++ classification/classifiers.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
 import random
 
+import openai
 
 class Classifier:
     @abstractmethod
@@ -13,5 +14,36 @@ class RandomClassifier(Classifier):
         return bool(random.getrandbits(1))
 
 
+class GPTClassifier(Classifier):
+    def __init__(self, model="gpt-3.5-turbo"):
+        self.model = model
+
+    def is_code_prompt(self, prompt: str) -> bool:
+        prompt_template = """
+Please determine whether the following user prompt is related to code or not:\n\n"
+{prompt}\n\n
+====
+If it's related to code, output "[[Y]]", if not, output "[[N]]". Please carefully follow this format.
+"""
+        convs = [
+            {"role": "system", "content": """
+Please determine whether the following user prompt is related to code or not. If it's related to code, output "[[Y]]", if not, output "[[N]]". Please carefully follow this format.
+"""},
+            {"role": "user", "content": prompt_template.format(prompt=prompt)},
+        ]
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=convs,
+            temperature=0,
+            max_tokens=512,
+        )
+        output = response["choices"][0]["message"]["content"]
+        if "[[Y]]" in output:
+            return True
+        elif "[[N]]" in output:
+            return False
+        else:
+            raise ValueError("Invalid response from GPT-3", output)
+
 # List of classifiers to use
-CLASSIFIERS = [RandomClassifier]
+CLASSIFIERS = [RandomClassifier, GPTClassifier]
--- classification/eval.py
+++ classification/eval.py
@@ -1,11 +1,24 @@
 import classifiers
 import pandas as pd
 from sklearn.metrics import f1_score
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
 
 data = pd.read_csv("classification/data.csv")
 
 for classifier_cls in classifiers.CLASSIFIERS:
     classifier = classifier_cls()
     print(f"Classifier: {classifier_cls.__name__}")
-    data["prediction"] = data["prompt"].apply(classifier.is_code_prompt)
-    print(f"F1 score: {f1_score(data['is_code_prompt'], data['prediction'])}")
+
+    with ThreadPoolExecutor(8) as executor:
+        future = []
+        index_list = []
+        for index, row in data.iterrows():
+            future.append(executor.submit(classifier.is_code_prompt, row["prompt"]))
+            index_list.append(index)
+        for idx, f in tqdm(enumerate(future)):
+            data.loc[index_list[idx], "prediction"] = f.result()
+
+    y_1 = data['is_code_prompt'].tolist()
+    y_2 = data['prediction'].tolist()
+    print(f"F1 score: {f1_score(y_1, y_2)}")
