bb0dd5f7c7d5a7fe070695383ff16f760f33b82c
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-11-28 00:14:41 +0000

    Add training script

---
+++ finetune/train.py
@@ -0,0 +1,268 @@
+# This code is based on tatsu-lab/stanford_alpaca. Below is the original copyright:
+#
+#    Copyright 2023 Rohan Taori, Ishaan Gulrajani, Tianyi Zhang, Yann Dubois, Xuechen Li
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import textwrap
+import os
+from dataclasses import dataclass, field
+import json
+import math
+import pathlib
+from typing import Dict, List, Optional, Sequence
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+import transformers
+from transformers import Trainer
+from transformers.trainer_pt_utils import LabelSmoother
+
+IGNORE_TOKEN_ID = LabelSmoother.ignore_index
+
+
+@dataclass
+class ModelArguments:
+    model_name_or_path: Optional[str] = field(default="facebook/opt-125m")
+    trust_remote_code: bool = field(
+        default=False,
+        metadata={
+            "help": "Whether or not to allow for custom models defined on the Hub in their own modeling files"
+        },
+    )
+    padding_side: str = field(
+        default="right", metadata={"help": "The padding side in tokenizer"}
+    )
+
+
+@dataclass
+class DataArguments:
+    data_path: str = field(
+        default=None, metadata={"help": "Path to the training data."}
+    )
+    eval_data_path: str = field(
+        default=None, metadata={"help": "Path to the evaluation data."}
+    )
+    lazy_preprocess: bool = False
+
+
+@dataclass
+class TrainingArguments(transformers.TrainingArguments):
+    cache_dir: Optional[str] = field(default=None)
+    optim: str = field(default="adamw_torch")
+    model_max_length: int = field(
+        default=512,
+        metadata={
+            "help": "Maximum sequence length. Sequences will be right padded (and possibly truncated)."
+        },
+    )
+
+
+local_rank = None
+
+
+def rank0_print(*args):
+    if local_rank == 0:
+        print(*args)
+
+
+def trainer_save_model_safe(trainer: transformers.Trainer):
+    from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+    from torch.distributed.fsdp import StateDictType, FullStateDictConfig
+
+    save_policy = FullStateDictConfig(offload_to_cpu=True, rank0_only=True)
+    with FSDP.state_dict_type(
+        trainer.model, StateDictType.FULL_STATE_DICT, save_policy
+    ):
+        trainer.save_model()
+
+
+PROGRAMMING_LABEL = "[[1. Programming]]"
+MATH_LABEL = "[[2. Math]]"
+NONE_LABEL = "[[3. None]]"
+
+
+def label(
+    raw_data: List[str],
+    label: str,
+) -> List[str]:
+    formatted_data = []
+    for query in raw_data:
+        formatted_query = textwrap.dedent(f"""
+            [INST] Determine whether the user query falls into one of the following categories:
+            1. Programming: Queries about programming languages, libraries, and tools.
+            2. Math: Queries about math, statistics, and data science.
+            3. None: Anything that does not fall into the above categories.
+            Your choice should be wrapped by “[[” and “]]”. For example, “[[3. None]]”.
+
+            [USER QUERY] {query!r}
+
+            [ANSWER] {label}"""
+        )
+        formatted_query = formatted_query.lstrip()
+        formatted_data.append(formatted_query)
+    return formatted_data
+
+
+def mask_label(
+    sources,
+    tokenizer: transformers.PreTrainedTokenizer,
+) -> Dict:
+    # Tokenize
+    input_ids = tokenizer(
+        sources,
+        return_tensors="pt",
+        padding="max_length",
+        max_length=tokenizer.model_max_length,
+        truncation=True,
+    ).input_ids
+    targets = input_ids.clone()
+
+    programming_label_len = len(tokenizer(PROGRAMMING_LABEL).input_ids)
+    math_label_len = len(tokenizer(MATH_LABEL).input_ids)
+    none_label_len = len(tokenizer(NONE_LABEL).input_ids)
+
+    # Mask targets. Only compute loss on the final outputs.
+    for example, target in zip(sources, targets):
+        total_len = int(target.ne(tokenizer.pad_token_id).sum())
+        if example.endswith(PROGRAMMING_LABEL):
+            label_len = programming_label_len
+        elif example.endswith(MATH_LABEL):
+            label_len = math_label_len
+        elif example.endswith(NONE_LABEL):
+            label_len = none_label_len
+        else:
+            raise ValueError(f"Invalid label: {example}")
+        target[:total_len - label_len] = IGNORE_TOKEN_ID
+
+        if False:  # Inspect and check the correctness of masking
+            z = target.clone()
+            z = torch.where(z == IGNORE_TOKEN_ID, tokenizer.unk_token_id, z)
+            rank0_print(tokenizer.decode(z))
+            exit()
+
+    return dict(
+        input_ids=input_ids,
+        labels=targets,
+        attention_mask=input_ids.ne(tokenizer.pad_token_id),
+    )
+
+
+class SupervisedDataset(Dataset):
+    """Dataset for supervised fine-tuning."""
+
+    def __init__(self, raw_data, tokenizer: transformers.PreTrainedTokenizer):
+        super(SupervisedDataset, self).__init__()
+
+        rank0_print("Formatting inputs...")
+        data_dict = mask_label(raw_data, tokenizer)
+
+        self.input_ids = data_dict["input_ids"]
+        self.labels = data_dict["labels"]
+        self.attention_mask = data_dict["attention_mask"]
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, i) -> Dict[str, torch.Tensor]:
+        return dict(
+            input_ids=self.input_ids[i],
+            labels=self.labels[i],
+            attention_mask=self.attention_mask[i],
+        )
+
+
+def make_supervised_data_module(
+    tokenizer: transformers.PreTrainedTokenizer, data_args
+) -> Dict:
+    """Make dataset and collator for supervised fine-tuning."""
+    dataset_cls = SupervisedDataset
+    rank0_print("Loading data...")
+
+    glaive_data = json.load(open(os.path.join(data_args.data_path, "glaive.json"), "r"))
+    glaive_data = label(glaive_data, PROGRAMMING_LABEL)
+    gsm_data = json.load(open(os.path.join(data_args.data_path, "gsm.json"), "r"))
+    gsm_data = label(gsm_data, MATH_LABEL)
+    orca_data = json.load(open(os.path.join(data_args.data_path, "orca.json"), "r"))
+    orca_data = label(orca_data, NONE_LABEL)
+    train_data = glaive_data + gsm_data + orca_data
+    train_dataset = dataset_cls(train_data, tokenizer=tokenizer)
+
+    if data_args.eval_data_path:
+        eval_json = json.load(open(data_args.eval_data_path, "r"))
+        eval_dataset = dataset_cls(eval_json, tokenizer=tokenizer)
+    else:
+        eval_dataset = None
+
+    return dict(train_dataset=train_dataset, eval_dataset=eval_dataset)
+
+
+def train():
+    global local_rank
+
+    parser = transformers.HfArgumentParser(
+        (ModelArguments, DataArguments, TrainingArguments)
+    )
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    local_rank = training_args.local_rank
+
+    config = transformers.AutoConfig.from_pretrained(
+        model_args.model_name_or_path,
+        cache_dir=training_args.cache_dir,
+        trust_remote_code=model_args.trust_remote_code,
+    )
+    config.use_cache = False
+
+    # Load model and tokenizer
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_args.model_name_or_path,
+        config=config,
+        cache_dir=training_args.cache_dir,
+        trust_remote_code=model_args.trust_remote_code,
+    )
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        model_args.model_name_or_path,
+        cache_dir=training_args.cache_dir,
+        model_max_length=training_args.model_max_length,
+        padding_side=model_args.padding_side,
+        use_fast=False,
+        trust_remote_code=model_args.trust_remote_code,
+    )
+
+    if tokenizer.pad_token != tokenizer.unk_token:
+        tokenizer.pad_token = tokenizer.unk_token
+
+    # Load data
+    data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
+
+    # Start trainner
+    trainer = Trainer(
+        model=model, tokenizer=tokenizer, args=training_args, **data_module
+    )
+    if list(pathlib.Path(training_args.output_dir).glob("checkpoint-*")):
+        trainer.train(resume_from_checkpoint=True)
+    else:
+        trainer.train()
+
+    # Save model
+    model.config.use_cache = True
+    trainer.save_state()
+    if trainer.is_deepspeed_enabled:
+        trainer.save_model()
+    else:
+        trainer_save_model_safe(trainer)
+
+
+if __name__ == "__main__":
+    train()
