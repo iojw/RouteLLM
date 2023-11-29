db23b6f3988a0d77a31f12e34af08807532feb0a
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2023-11-29 01:16:57 +0000

    Minor

--- router_performance.py
+++ router_performance.py
@@ -16,18 +16,19 @@ PERFORMANCE2 = {
     "Embedding": 0.966,
     "Zero-shot\nLlama2-7B": 0.5,
     "Finetuned\nLlama2-7B": 0.833,
-    "Zero-shot\nGPT-3.5-Turbo": 1.0,
+    "Zero-shot\nGPT-3.5": 1.0,
 }
 
 
 def draw_performance(performance, title, color):
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(11, 6))
     plt.bar(performance.keys(), performance.values(), color=color)
+    # plt.axhline(y=0.33, color="black", linestyle="--")
     for k, v in performance.items():
-        plt.text(k, v + 0.01, f"{v:.2f}", ha="center", fontsize=13)
+        plt.text(k, v + 0.01, f"{v:.2f}", ha="center", fontsize=15)
     plt.ylim(0, 1.1)
-    plt.xticks(fontsize=13)
-    plt.ylabel("F1 score", fontsize=13)
+    plt.xticks(fontsize=18)
+    plt.ylabel("F1 score", fontsize=15)
     plt.tight_layout()
     plt.savefig(f"{title}.png", dpi=600)
 
